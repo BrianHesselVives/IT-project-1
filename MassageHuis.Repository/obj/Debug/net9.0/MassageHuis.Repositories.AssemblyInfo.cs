bf6596c6df8452d702a54d32bdd692d3f84d49f5
Brian Hessel <brian.hessel@student.vives.be>
2025-05-02 18:01:11 +0200

    Sprint 3: Update naamgeving entities en massagehuisdbcontext

--- MassageHuis.Repository/obj/Debug/net9.0/MassageHuis.Repositories.AssemblyInfo.cs
+++ MassageHuis.Repository/obj/Debug/net9.0/MassageHuis.Repositories.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MassageHuis.Repositories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26986d58f107e584f7547b548e458b7be4e50c2f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cc46337157cbae95c1f8615fec8068de77075e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("MassageHuis.Repositories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MassageHuis.Repositories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
