5d3ffd37d07403ce01c0d6733cea190c2e7097ac
Brian Hessel <brian.hessel@student.vives.be>
2025-05-02 18:01:42 +0200

    Sprint 3: Update naamgeving entities en massagehuisdbcontext

---
+++ MassageHuis.Domain/Entities/KostPrijs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MassageHuis.Entities;
+
+public partial class KostPrijs
+{
+    public int Id { get; set; }
+
+    public float Prijs { get; set; }
+
+    public DateOnly Startdatum { get; set; }
+
+    public int IdTypeMassage { get; set; }
+
+    public virtual TypeMassage IdTypeMassageNavigation { get; set; } = null!;
+
+    public virtual ICollection<Reservatie> Reservaties { get; set; } = new List<Reservatie>();
+}
