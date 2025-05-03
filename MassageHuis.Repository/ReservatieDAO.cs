17d131a8e15cc6df3e720dc1a8da3b15978eca7a
Brian Hessel <brian.hessel@student.vives.be>
2025-05-03 11:59:45 +0200

    Sprint 3: Toevoegen van logica om tijdsloten te weergeven

---
+++ MassageHuis.Repository/ReservatieDAO.cs
@@ -0,0 +1,56 @@
+﻿using MassageHuis.Repositories.Interfaces;
+using System.Diagnostics;
+using MassageHuis.Entities;
+using Microsoft.EntityFrameworkCore;
+using MassageHuis.Data;
+
+namespace MassageHuis.Repositories
+{
+    public class ReservatieDAO : IDAO<Reservatie>
+    {
+        private readonly MassageHuisDbContext _dbContext;
+        public ReservatieDAO(MassageHuisDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        async public Task<Reservatie?> FindByIdAsync(Reservatie entity)
+        {
+            throw new NotImplementedException();
+        }
+        
+
+        Task IDAO<Reservatie>.AddAsync(Reservatie entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task IDAO<Reservatie>.DeleteAsync(Reservatie entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        async Task<IEnumerable<Reservatie>?> IDAO<Reservatie>.GetAllAsync()
+        {
+            try
+            {
+                return await _dbContext.Reservaties.ToListAsync();
+            }
+            catch (Microsoft.Data.SqlClient.SqlException ex)
+            {
+                Debug.WriteLine("db error:", ex.ToString());
+                return null;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                return null;
+            }
+        }
+
+        Task IDAO<Reservatie>.UpdateAsync(Reservatie entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MassageHuis.Service/ReservatieService.cs
@@ -0,0 +1,45 @@
+﻿using MassageHuis.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MassageHuis.Entities;
+using MassageHuis.Repositories.Interfaces;
+
+namespace MassageHuis.Services
+{
+    public class ReservatieService : IService<Reservatie>
+    {
+        private IDAO<Reservatie> _reservatieDAO;
+        public ReservatieService(IDAO<Reservatie> reservatieDAO)
+
+        {
+            _reservatieDAO = reservatieDAO;
+        }
+        public async Task AddAsync(Reservatie entity)
+        {
+            await _reservatieDAO.AddAsync(entity);
+        }
+
+        public async Task DeleteAsync(Reservatie entity)
+        {
+            await _reservatieDAO.DeleteAsync(entity);
+        }
+
+        public async Task<Reservatie?> FindByIdAsync(Reservatie id)
+        {
+            return await _reservatieDAO.FindByIdAsync(id);
+        }
+
+        public async Task<IEnumerable<Reservatie>?> GetAllAsync()
+        {
+            return await _reservatieDAO.GetAllAsync();
+        }
+
+        public Task UpdateAsync(Reservatie entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
