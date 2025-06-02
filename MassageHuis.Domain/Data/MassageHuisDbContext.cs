edaf4cef2c6eed07871b68875c20d97044254bbe
Brian Hessel <brian.hessel@student.vives.be>
2025-06-02 21:38:19 +0200

    Aanpassing git ignore

--- MassageHuis.Domain/Data/MassageHuisDbContext.cs
+++ MassageHuis.Domain/Data/MassageHuisDbContext.cs
@@ -50,9 +50,9 @@ public partial class MassageHuisDbContext : DbContext
 
     public virtual DbSet<UitzonderingTijdslot> UitzonderingTijdslots { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=.\\SQL24_VIVES; Database=Massagehuis; Trusted_Connection=True; TrustServerCertificate=True; MultipleActiveResultSets=true;");
+      // => optionsBuilder.UseSqlServer("Server=.\\SQL24_VIVES; Database=Massagehuis; Trusted_Connection=True; TrustServerCertificate=True; MultipleActiveResultSets=true;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
